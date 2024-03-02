f53a80963efc97f80185a65e5f8a60967fea6705
Vidyanand Kumar <141124787+vidyanandk@users.noreply.github.com>
2024-03-02 12:09:59 +0530

    Update LogoutG.js

--- src/components/LogoutG.js
+++ src/components/LogoutG.js
@@ -1,20 +1,20 @@
-import {GoogleLogin, GoogleLogout} from 'react-google-login'
-const clientId = "1074289233883-2u3i89rurj2tb9a1ctl923ntuv5fq6as.apps.googleusercontent.com"
+// import {GoogleLogin, GoogleLogout} from 'react-google-login'
+// const clientId = "1074289233883-2u3i89rurj2tb9a1ctl923ntuv5fq6as.apps.googleusercontent.com"
 
-function LogoutG(){
+// function LogoutG(){
 
-    const onSuccess = () =>{
-        console.log("Logout successful")
-    }
-    return (
-        <div className="signOutButton">
-            <GoogleLogout
-                clientId={clientId}
-                buttonText={'Logout'}
-                onLogoutSuccess={onSuccess}
-            />
-        </div>
-    )
-}
+//     const onSuccess = () =>{
+//         console.log("Logout successful")
+//     }
+//     return (
+//         <div className="signOutButton">
+//             <GoogleLogout
+//                 clientId={clientId}
+//                 buttonText={'Logout'}
+//                 onLogoutSuccess={onSuccess}
+//             />
+//         </div>
+//     )
+// }
 
-export default LogoutG;
+// export default LogoutG;
